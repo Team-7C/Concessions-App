baa2895cf03511e14214ec1efff40c9b3f9455f0
dsantonil <dsantonil@ufl.edu>
2019-11-14 12:31:51 -0500

    edited home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,15 +2,42 @@ import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import './Home.css'
+import {Layout, Header, Navigation, Drawer, Content, Grid, Cell} from 'react-mdl';
 
 class App extends React.Component {
 
     render() {
         return (
-                  <div className="page-content">
-                    <img src={ require('../assets/home_food.jpg') } />
-                  </div>
+              <div className="page-content">
+                <img 
+                 src={ require('../assets/home_food.jpg') } 
+                 alt="food background"
+                 className = "background-transparent" 
+                />
+                <div>
+                  <img
+                    src={ require('../assets/Concession_Stand.jpg')}
+                    alt="concession"
+                    className = "logo"
+                  />
+                </div>
+
+                <div className = "banner-text">
+                  <h1>
+                    Team 7C Concession App. 
+                  </h1>
+                  <hr/>
+                  <p>
+                    It's like Grubhub, but not.
+                  </p>
+                </div>
+
+                <div>
+                  
+                </div>
+              </div>
+              
         );
     }
 }
