a383a49152792732d1177a1a0c07437df3140e61
Linkraft <36479127+Linkraft@users.noreply.github.com>
2019-11-29 14:17:25 -0500

    Started fixing transaction data

--- server/models/transactionSchema.js
+++ server/models/transactionSchema.js
@@ -4,25 +4,30 @@ var mongoose = require('mongoose'),
 
 // Create your schema for the data in the listings.json file that will define how data is saved in your database
 var transactionSchema = new Schema({
-  id: { type: Number, required: true, unique: true},
-  cid: Number,
-  vid: Number,
-  itemInfo: [ {id: Number , addOns: Boolean} ],
-  addOns: [String],
-  price: Number,
-  time: {type: Date, default: Date.now }, 
+  tid: {type: Number, required: [true, 'Transaction ID is required'], unique: [true, 'Transaction ID must be unique']},
+  uid: {type: Number, required: [true, "Customer ID is required"] }, // Customer who made transaction
+  items: [
+    {
+      id: Number,                            // Unique item
+      quantity: {type: Number, default: 1},  // Number of instances in transaction
+      addons: [{aid: Number}]                // Addons (that have upcharges) applied to item
+    }
+  ],
+  subtotal: { type: Number, required: [true, 'Transaction subtotal is required'] },
+  time: {type: Date, default: Date.now}, 
   created_at: Date,
   updated_at: Date
 });
 
 // Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
 transactionSchema.pre('save', function(next) {
-        var curr = new Date();
-        this.updated_at = curr;
-        if(!this.created_at)
-                this.created_at = curr;
-        next();
+  var curr = new Date();
+  this.updated_at = curr;
+  if(!this.created_at)
+    this.created_at = curr;
+  next();
 });
+
 /* Use your schema to instantiate a Mongoose model */
 var Transaction = mongoose.model('Transaction', transactionSchema);
 
